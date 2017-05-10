f503842ebd945e7a4a8b0b66bbaa98dc4d401a21
SeomGi, Han <iandmyhand@gmail.com>
2017-05-10 10:54:41 +0900

    Update PDF compressor to use just filename.

--- compressor.py
+++ compressor.py
@@ -7,19 +7,22 @@ from PyPDF2 import PdfFileWriter
 
 class PDFCompressor(object):
 
-    def compress(self, file_full_path):
-        print("Compressor starts with [{}].".format(file_full_path))
-        if not os.path.isfile(file_full_path):
-            print("[{}] is not a file.".format(file_full_path))
+    def compress(self, file_path):
+        if not file_path.startswith('/'):
+            file_path = '{}/{}'.format(os.path.dirname(os.path.realpath(__file__)), file_path)
+
+        print("Compressor starts with [{}].".format(file_path))
+        if not os.path.isfile(file_path):
+            print("[{}] is not a file.".format(file_path))
             return
 
-        original_file_size = os.path.getsize(file_full_path)
-        backup_path = file_full_path + '.bak'
+        original_file_size = os.path.getsize(file_path)
+        backup_path = file_path + '.bak'
         if os.path.isfile(backup_path):
             print("Failed to back up original file...")
             return
 
-        with open(file_full_path, 'rb') as original_file:
+        with open(file_path, 'rb') as original_file:
             with open(backup_path, 'wb') as backup_file:
                 backup_file.write(original_file.read())
 
@@ -29,11 +32,11 @@ class PDFCompressor(object):
             original_page = reader.getPage(i)
             original_page.compressContentStreams()
             writer.addPage(original_page)
-        writer.write(open(file_full_path, 'wb'))
+        writer.write(open(file_path, 'wb'))
 
-        compressed_file_size = os.path.getsize(file_full_path)
+        compressed_file_size = os.path.getsize(file_path)
 
-        print("Compressor ends with [{}].".format(file_full_path))
+        print("Compressor ends with [{}].".format(file_path))
         print("Original File size:{:,}KB".format(int(original_file_size / 1024)))
         print("Compressed File size:{:,}KB".format(int(compressed_file_size / 1024)))
         return
@@ -41,7 +44,7 @@ class PDFCompressor(object):
 
 if __name__ == '__main__':
     if 2 > len(sys.argv):
-        print("Please put file full path.")
-        print("Usage: $ python3 compressor.py [FILE_FULL_PATH]")
+        print("Please put file path.")
+        print("Usage: $ python3 compressor.py [FILE_PATH]")
     else:
         PDFCompressor().compress(sys.argv[1])
